32a1efc19ebea01e8bbabf95d4828ae419bcba95
Leblayd <leblaydavid7@gmail.com>
2018-05-22 16:55:04 +0200

    fuzc

--- player.py
+++ player.py
@@ -1,49 +1,8 @@
-import matcher as cards_matcher
-
 class Player:
-    VERSION = "ez nem commit, ez a version name"
-
-    def __init__(self):
-        self.me, self.card1, self.card2, self.value = None, None, None, None
-        self.score = 0
-        self.cards = None
-        self.common_cards = None
-        self.matcher = None
-
-    def get_cards(self, game_state):
-        self.common_cards = game_state["community_cards"]
-        for player in game_state["players"]:
-            if player["name"] == "Texas Rangers":  # this is us
-                self.me = player
-                self.cards = self.me["hole_cards"]
-                if self.cards[0] is not None:
-                    self.score += self.matcher.get_rank_value(self.cards[0])
-                if self.cards[1] is not None:
-                    self.score += self.matcher.get_rank_value(self.cards[1])
-
-        self.matcher = cards_matcher.Matcher(self.cards, self.common_cards, self)
+    VERSION = "fuck"
 
     def betRequest(self, game_state):
-        self.value = game_state["current_buy_in"]
+        return 10000000
 
-        self.get_cards(game_state)
-
-        if game_state["round"] == 0:
-            self.value += 10
-            self.value *= (self.matcher.find_match() + 0)
-        elif game_state["round"] == 1:
-            self.value *= (self.matcher.find_match() + 0)
-        elif game_state["round"] == 2:
-            self.value *= self.matcher.find_match()
-        elif game_state["round"] == 3:
-            self.value *= self.matcher.find_match()
-
-        if game_state["current_buy_in"] >= self.me["stack"]:
-            self.value = 0
-
-        print self.value
-        return self.value
-        print "IABSNBJADAF AWFHBAHIJFUJOAWBFJOANDBAHIJFHAWBDFAHBCFAWS IDFAW FUI AWIFIOWASB FICAH IFWAQ AW"
-        return game_state["current_buy_in"]
     def showdown(self, game_state):
         pass
