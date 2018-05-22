fc0ab6801f628821b6cc47bec1bb8a0c273db2f7
Leblayd <leblaydavid7@gmail.com>
2018-05-22 10:43:17 +0200

    some tweaking

--- player.py
+++ player.py
@@ -6,11 +6,12 @@ class Player:
     def betRequest(self, game_state):
         for player in game_state["players"]:
             if player["name"] == "Texas Rangers":
-                print player
+                for card in player["hole_cards"]:
+                    print card
 
         if game_state["round"] == 0:
-            return 10
-        return 1
+            return 50
+        return game_state["current_buy_in"] + 2
 
     def showdown(self, game_state):
         pass
