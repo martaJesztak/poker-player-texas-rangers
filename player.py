78123cfd71afa46894dde3a9c2b2e8751ab7dda8
Leblayd <leblaydavid7@gmail.com>
2018-05-22 11:39:37 +0200

    fix again

--- player.py
+++ player.py
@@ -1,11 +1,8 @@
-import json
-
-
 class Player:
     VERSION = "Default Python folding player"
 
     def __init__(self):
-        self.me, self.card1, self.card2, self.bet = None, None, None, None
+        self.me, self.card1, self.card2, self.value = None, None, None, None
 
     def get_cards(self, game_state):
         for player in game_state["players"]:
@@ -15,14 +12,14 @@ class Player:
                 self.card2 = self.me["hole_cards"][1]
 
     def betRequest(self, game_state):
-        self.bet = game_state["current_buy_in"]
+        self.value = game_state["current_buy_in"]
 
         self.get_cards(game_state)
 
-        value = self.if_pair(value)
+        self.if_pair()
 
         if game_state["round"] == 0:
-            return value + 20
+            return self.value + 20
         elif game_state["round"] == 1:
             pass
         elif game_state["round"] == 2:
@@ -32,19 +29,18 @@ class Player:
         else:
             pass
 
-        if value > 500:
+        if self.value > 500:
             return 0
 
-        return value + 2
+        return self.value + 2
 
-    def if_pair(self, value):
+    def if_pair(self):
         if self.card1 == self.card2:
-            value = self.pair(value)
-        return value
+            self.value = self.pair_action()
 
-    def pair(self, value):
-        value += 20
-        return value
+    def pair_action(self):
+        self.value += 20
+        return self.value
 
     def showdown(self, game_state):
         pass
