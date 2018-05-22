b0653756278411148b52ae51563e1fa9e2d0ccf3
Leblayd <leblaydavid7@gmail.com>
2018-05-22 10:24:23 +0200

    import json

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
+import json
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print game_state
+        print json.load(game_state)
         return 0
 
     def showdown(self, game_state):
