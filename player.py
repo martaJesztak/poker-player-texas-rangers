97c55e18e48e685b185426e04e1e406f71d44875
Leblayd <leblaydavid7@gmail.com>
2018-05-22 13:42:03 +0200

    put error handling

--- player.py
+++ player.py
@@ -12,38 +12,40 @@ class Player:
                 self.card2 = self.me["hole_cards"][1]
 
     def betRequest(self, game_state):
-        self.value = game_state["current_buy_in"]
+        try:
+            self.value = game_state["current_buy_in"]
 
-        self.get_cards(game_state)
+            self.get_cards(game_state)
 
-        if game_state["round"] == 0:
-            self.value += 20
-        elif game_state["round"] == 1:
-            pass
-        elif game_state["round"] == 2:
-            pass
-        elif game_state["round"] == 3:
-            pass
-        else:
-            pass
+            if game_state["round"] == 0:
+                self.value += 20
+            elif game_state["round"] == 1:
+                pass
+            elif game_state["round"] == 2:
+                pass
+            elif game_state["round"] == 3:
+                pass
+            else:
+                pass
 
-        print self.card1
-        print self.card2
-        if self.card1 == self.card2:
-            self.pair_action()
+            print self.card1
+            print self.card2
+            if self.card1 == self.card2:
+                self.pair_action()
 
-        if game_state["current_buy_in"] >= self.me["stack"]:
-            self.all_in_action()
+            if game_state["current_buy_in"] >= self.me["stack"]:
+                self.all_in_action()
 
-        print self.value
+            print self.value
 
-        return self.value
+            return self.value
+        except IOError:
+            return game_state["current_buy_in"]
 
     def pair_action(self):
         self.value += 20
 
     def all_in_action(self):
-
         self.value = 10000
 
     def showdown(self, game_state):
