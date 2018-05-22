9271c17f27712dca90bead602337ff047236474a
Leblayd <leblaydavid7@gmail.com>
2018-05-22 10:33:12 +0200

    test3

--- player.py
+++ player.py
@@ -4,8 +4,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        for card in game_state["players"]["hole_cards"]:
-            print card
+        for player in game_state["players"]:
+            print player
 
         return 0
 
