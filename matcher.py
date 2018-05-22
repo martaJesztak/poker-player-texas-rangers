a49550fd4893c417c1ffcdedc142d931bfb2e852
J-Abel <farinae.subactae@gmail.com>
2018-05-22 15:20:54 +0200

    finding pair, two pair, drill

--- matcher.py
+++ matcher.py
@@ -11,13 +11,38 @@ class Matcher:
         print self.common_cards
 
     def if_pair(self):
-        for i, card in enumerate(self.cards):
-            for j, common in enumerate(self.common_cards):
+        for i, card in enumerate(self.all_cards):
+            for j, common in enumerate(self.all_cards):
                 if card == common:
                     return True
         return False
 
+    def if_two_pair(self):
+        pairs_found = 0
+        for i, card in enumerate(self.all_cards):
+            for j, common in enumerate(self.all_cards):
+                if card == common:
+                    pairs_found += 1
+        if pairs_found >= 2:
+            return True
+        return False
+
+    def if_drill(self):
+        same_rank_found = 0
+        for i, card in enumerate(self.all_cards):
+            for j, common in enumerate(self.all_cards):
+                if card == common:
+                    same_rank_found += 1
+        if same_rank_found >= 3:
+            return True
+        return False
+
+
     def find_match(self):
+        if self.if_drill():
+            return 1.2
+        if self.if_two_pair():
+            return 1.1
         if self.if_pair():
             return 1
         return 0
