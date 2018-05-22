58714a619b9a3d406c022c1caa568d246f2963ef
Leblayd <leblaydavid7@gmail.com>
2018-05-22 10:52:36 +0200

    actually introduced some logic

--- player.py
+++ player.py
@@ -1,18 +1,23 @@
 import json
 
+
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        value = game_state["current_buy_in"]
+
         for player in game_state["players"]:
-            if player["name"] == "Texas Rangers":
-                for card in player["hole_cards"]:
-                    print card
+            if player["name"] == "Texas Rangers":  # this is us
+
+                if player["hole_cards"][0]["rank"] == player["hole_cards"][1]["rank"]:
+                    value += 20
+
+            print player["status"]
 
         if game_state["round"] == 0:
-            return 50
-        return game_state["current_buy_in"] + 2
+            return value + 20
+        return value + 2
 
     def showdown(self, game_state):
         pass
-
